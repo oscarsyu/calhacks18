b30c94a0d2ca0d5e081dc4a5e450cea179e9a003
Zhuo Lu <sethlu@mintkit.net>
2018-11-02 23:57:51 -0700

    Add some mock data for front end use

--- server/app/app.py
+++ server/app/app.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, json
 
 bp = Blueprint('app', __name__)
 
@@ -6,3 +6,25 @@ bp = Blueprint('app', __name__)
 @bp.route('/')
 def index():
     return 'Hey this is working!'
+
+
+@bp.route('/mock/playlists/<string:mood>')
+def mock_playlist(mood):
+    """
+    Some mock data available at
+        /mock/playlists/happy
+        /mock/playlists/sad
+    """
+
+    mock_playlists = {
+        'happy': [
+            'Happy song 1',
+            'Happy song 2',
+        ],
+        'sad': [
+            'Sad song 1',
+            'Sad song 2',
+        ],
+    }
+
+    return json.dumps(mock_playlists[mood] if mood in mock_playlists else [])
