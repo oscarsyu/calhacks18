f9bcf5820e3bd2e45f63172d1a893047ed5cd244
Adrian Jules Sales <ajsales@berkeley.edu>
2018-11-04 01:49:07 -0700

    took out nlp

--- spotify/language_methods.py
+++ spotify/language_methods.py
@@ -38,56 +38,8 @@ def mood_checker(text):
 			if token in moods[mood]:
 				return mood;
 
-def sample_analyze_sentiment(content):
-
-    client = language.LanguageServiceClient()
-
-    # content = 'Your text to analyze, e.g. Hello, world!'
-
-    if isinstance(content, six.binary_type):
-        content = content.decode('utf-8')
-
-    type_ = enums.Document.Type.PLAIN_TEXT
-    document = {'type': type_, 'content': content}
-
-    response = client.analyze_sentiment(document)
-    sentiment = response.document_sentiment
-    print('Score: {}'.format(sentiment.score))
-    print('Magnitude: {}'.format(sentiment.magnitude))
-
-def entity_sentiment_text(text):
-    """Detects entity sentiment in the provided text."""
-    client = language.LanguageServiceClient()
-
-    if isinstance(text, six.binary_type):
-        text = text.decode('utf-8')
-
-    document = types.Document(
-        content=text.encode('utf-8'),
-        type=enums.Document.Type.PLAIN_TEXT)
-
-    # Detect and send native Python encoding to receive correct word offsets.
-    encoding = enums.EncodingType.UTF32
-    if sys.maxunicode == 65535:
-        encoding = enums.EncodingType.UTF16
-
-    result = client.analyze_entity_sentiment(document, encoding)
-
-    for entity in result.entities:
-        print('Mentions: ')
-        print(u'Name: "{}"'.format(entity.name))
-        for mention in entity.mentions:
-            print(u'  Begin Offset : {}'.format(mention.text.begin_offset))
-            print(u'  Content : {}'.format(mention.text.content))
-            print(u'  Magnitude : {}'.format(mention.sentiment.magnitude))
-            print(u'  Sentiment : {}'.format(mention.sentiment.score))
-            print(u'  Type : {}'.format(mention.type))
-        print(u'Salience: {}'.format(entity.salience))
-        print(u'Sentiment: {}\n'.format(entity.sentiment))
-
-
 def main():
-	entity_sentiment_text("sad in the feels R&B")
+	sample_analyze_sentiment("sad R&B")
 
 
 if __name__ == '__main__': main()
